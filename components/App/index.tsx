78f11d6ce58eab696f09ed5650e648699f19ac16
Stuart Thomson <me@sthom.kiwi>
2019-07-20 20:03:38 +1200

    Added global CSS

--- components/App/index.tsx
+++ components/App/index.tsx
@@ -3,6 +3,8 @@ import App, { Container, AppInitialProps } from 'next/app';
 import { NextComponentType, NextPageContext } from 'next';
 import Header from '../Header';
 
+import './index.global.css';
+
 interface InitialPropsProps {
   Component: NextComponentType<NextPageContext, any, {}>;
   ctx: NextPageContext;
