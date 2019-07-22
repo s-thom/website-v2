6ef50f06251b9804c69dc18b9ac7d29e9b6f7ae5
Stuart Thomson <me@sthom.kiwi>
2019-07-22 20:03:59 +1200

    Un-feature some posts
    You know, I've been wanting to have this functionality for a while, it's nice to have it

--- types.d.ts
+++ types.d.ts
@@ -1,16 +1,3 @@
-export interface PageMetaBase {
-  title: string;
-  bgcolor: string;
-  img?: string;
-}
-
-export interface PageMeta extends PageMetaBase {
-  github?: string;
-  date?: string;
-  edited?: string;
-  url?: string;
-}
-
 export interface PageMetadata {
   title: string;
   published?: boolean;
