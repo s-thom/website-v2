8158ee2b24d0f48e5d69d904c4b2e7f2eec8aca6
Stuart Thomson <me@sthom.kiwi>
2019-10-25 20:27:04 +1300

    Basic start on importantness

--- components/MdImage/index.tsx
+++ components/MdImage/index.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 import './index.css';
 
+interface OptionalProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  nocaption?: boolean;
+}
+
 export default function MdImage(props: React.ImgHTMLAttributes<HTMLImageElement>) {
+  if ((props as OptionalProps).nocaption) {
+    const clone = {...props};
+    delete (clone as OptionalProps).nocaption;
+    return <img  {...props} />;
+  }
+
   const caption = props.title || props.alt;
   return (
     <figure className="FigureImage">
