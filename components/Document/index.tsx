757a956f917750bd3b56206da317f48e33e504ab
Stuart Thomson <me@sthom.kiwi>
2019-11-23 21:28:09 +1300

    Add lang to <html>

--- components/Document/index.tsx
+++ components/Document/index.tsx
@@ -4,7 +4,10 @@ import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 export default class Document extends NextDocument {
   public render() {
     return (
-      <Html prefix="og: http://ogp.me/ns# article: http://ogp.me/ns/article">
+      <Html
+        prefix="og: http://ogp.me/ns# article: http://ogp.me/ns/article"
+        lang="en"
+      >
         <Head />
         <body>
           <Main />
